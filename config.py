670b42bccee4c4efe7e72f024750999b6b336ae8
Quang Dao <dnquang2588@gmail.com>
2025-03-04 21:08:41 -0800

    initial commit

--- config.py
+++
@@ -1,7 +0,0 @@
-# Spotify
-CLIENT_ID = 'a05496eeccca4af39ee0ee98ed1dcd2b'
-CLIENT_SECRET = '3eb5748a011e4304804571da4dd62305'
-REDIRECT_URI = 'http://127.0.0.1:8888/callback'
-
-# MongoDB
-MONGO_URI = 'mongodb+srv://Thaipham:QuangThai@@thaipham.cqg8w.mongodb.net/sql-cloud?retryWrites=true&w=majority'
